393aa4af934a87b828a5017b28f68c8cdc4e9f29
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-10-11 20:18:45 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+@@all =[]
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |song|
+      puts song.name
+    end
+  end
+end
